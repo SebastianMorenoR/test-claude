8bd9f2c7e35756d3f8cd50e8889d0f6341d4ffe1
SebastianMorenoR <jusemore94@gmail.com>
2026-04-06 11:39:34 -0500

    feat: primer commit con archivos de practica

---
+++ error-prueba.js
@@ -0,0 +1,13 @@
+// Esta línea intenta mostrar en consola el valor de una variable llamada 'nombreQueNoExiste'.
+// Sin embargo, esta variable no ha sido declarada ni definida previamente en el código.
+// Por eso, al ejecutar esta línea se produce un error de referencia (ReferenceError),
+// indicando que 'nombreQueNoExiste' no está definida.
+console.log(nombreQueNoExiste)
+
+function mayorDeDos(a, b) {
+  return a > b ? a : b;
+}
+
+function menorDedos(a, b) {
+  return a < b ? a : b;
+}
