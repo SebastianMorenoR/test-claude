25dc0e7dc375d703b296a2a3febcdebd8c8473e0
SebastianMorenoR <jusemore94@gmail.com>
2026-04-07 23:20:54 -0500

    feat: agrega archivo calculadora

---
+++ calculadora.js
@@ -0,0 +1,25 @@
+function sumar(a, b) {
+    return a + b;
+}
+
+function restar(a, b) {
+    return a - b;
+}
+
+function multiplicar(a, b) {
+    return a * b;
+}
+
+function dividir(a, b) {
+    return a / b;
+}
+
+function potencia(a, b) {
+    return a ** b;
+}
+
+function raiz(a) {
+    return Math.sqrt(a);
+}
+
+function logaritmo(a) {
